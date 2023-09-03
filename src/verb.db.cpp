de8b130a0ea9de16849f9b05f84a3c1da1ece1b5
Timo Früh <timo.frueh@icloud.com>
2023-09-03 13:51:17 +0200

    add full version of acheter

--- src/verb.db.cpp
+++ src/verb.db.cpp
@@ -6,4 +6,71 @@
 
 #include "verb.db.hpp"
 
-verbDB::verb acheter;
+verbDB::Verb verbDB::acheter = {
+    typeER,
+
+    "acheter",
+    "achetant",
+
+    "achète",
+    "achètes",
+    "achète",
+    "achète",
+    "achetons",
+    "achetez",
+    "achètent",
+    "achètent",
+
+    "achetais",
+    "achetais",
+    "achetait",
+    "achetait",
+    "achetions",
+    "achetiez",
+    "achetaient",
+    "achetaient"
+
+    "achèterai",
+    "achèteras",
+    "achètera",
+    "achètera",
+    "achèterons",
+    "achèterez",
+    "achèteront",
+    "achèteront"
+
+    "ai acheté",
+    "as acheté",
+    "a acheté",
+    "a acheté",
+    "avons acheté",
+    "avez acheté",
+    "ont acheté",
+    "ont acheté"
+
+    "avais acheté",
+    "avais acheté",
+    "avait acheté",
+    "avait acheté",
+    "avions acheté",
+    "aviez acheté",
+    "avaient acheté",
+    "avaient acheté",
+
+    "achète",
+    "achètes",
+    "achète",
+    "achète",
+    "achetions",
+    "achetiez",
+    "achètent",
+    "achètent",
+
+    "achèterais",
+    "achèterais",
+    "achèterait",
+    "achèterait",
+    "achèterions",
+    "achèteriez",
+    "achèteraient",
+};

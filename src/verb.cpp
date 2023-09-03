897869f0ec732401b550aeb6f25157316c0cc5c6
Timo Früh <timo.frueh@icloud.com>
2023-09-03 18:44:13 +0200

    update displayVerb

--- src/verb.cpp
+++ src/verb.cpp
@@ -11,11 +11,21 @@
 void displayVerb(verbDB::Verb verb) {
     std::string out;
 
-    out.append(verb.infinitif + "\n");
+    out.append("+" + std::string(verb.infinitif.length(), '-') + "+\n");
+    out.append("|" + verb.infinitif + "|\n");
+    out.append("+" + std::string(verb.infinitif.length(), '-') + "+\n");
+
     out.append("\n");
     out.append("présent\n");
     out.append("-------\n");
     out.append("je/j' " + verb.presentJe + "\n");
+    out.append("tu" + verb.presentTu + "\n");
+    out.append("il" + verb.presentIl + "\n");
+    out.append("elle" + verb.presentElle + "\n");
+    out.append("nous" + verb.presentNous + "\n");
+    out.append("vous" + verb.presentVous + "\n");
+    out.append("ils" + verb.presentIls + "\n");
+    out.append("elles" + verb.presentElles + "\n");
 
     std::cout << out;
 }

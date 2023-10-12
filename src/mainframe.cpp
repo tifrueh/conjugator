581590a5c6f282c1ec694f17114854e139b4669e
Timo Früh <timo.frueh@icloud.com>
2023-10-12 16:53:11 +0200

    add even more information to the GTK about box

--- src/mainframe.cpp
+++ src/mainframe.cpp
@@ -29,6 +29,23 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title) {
     #ifdef __WXGTK__
         info.SetIcon(wxICON(conjugateur));
         info.SetDescription(wxT("Entraîneur de conjugaison des verbes français"));
+        info.SetWebSite(wxT("https://github.com/tifrueh/conjugateur"), wxT("GitHub"));
+        info.AddDeveloper(wxT("Timo Früh"));
+
+        info.SetLicense(wxT("This program is free software: you can redistribute it and/or modify "
+                            "it under the terms of the GNU General Public License as published by "
+                            "the Free Software Foundation, either version 3 of the License, or "
+                            "(at your option) any later version.\n"
+                            "\n"
+                            "This program is distributed in the hope that it will be useful, "
+                            "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+                            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+                            "GNU General Public License for more details.\n"
+                            "\n"
+                            "You should have received a copy of the GNU General Public License "
+                            "along with this program.  If not, see <https://www.gnu.org/licenses/>."));
+
+
         info.AddTranslator(wxT("Stephane Junique"));
         info.AddTranslator(wxT("Nicolas Velin"));
         info.AddTranslator(wxT("Gérard Durand"));

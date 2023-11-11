953eb6f4137c1530b7b898f69a67b157d8de8ff5
Timo Früh <timo.frueh@icloud.com>
2023-11-11 12:50:46 +0100

    use meson environment in mac bundle script

---
+++ scripts/bundle_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd "${MESON_SOURCE_ROOT}"
+
+MESON_RES_PATH="${MESON_SOURCE_ROOT}/resources"
+MESON_LC_FR_PATH="${MESON_RES_PATH}/lang/fr/LC_MESSAGES"
+
+APP_PATH="${MESON_BUILD_ROOT}/Conjugateur.app"
+CONTENTS_PATH="${APP_PATH}/Contents"
+RES_PATH="${CONTENTS_PATH}/Resources"
+EXE_PATH="${CONTENTS_PATH}/MacOS"
+LC_FR_PATH="${RES_PATH}/fr.lproj"
+
+rm -rf "${APP_PATH}"
+mkdir "${APP_PATH}"
+
+install -d -m 755 "${CONTENTS_PATH}" "${RES_PATH}" "${EXE_PATH}" "${LC_FR_PATH}"
+
+install -m 644 "${MESON_RES_PATH}/Info.plist" "${CONTENTS_PATH}"
+install -m 644 "${MESON_RES_PATH}/conjugateur.icns" "${RES_PATH}"
+install -m 644 "${MESON_LC_FR_PATH}/wxstd.mo" "${LC_FR_PATH}"
+install -s -m 755 "${MESON_BUILD_ROOT}/conjugateur" "${EXE_PATH}"
+

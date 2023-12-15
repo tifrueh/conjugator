933936af2754a08c62f40eff71c7be25c50e8347
Timo Früh <timo.frueh@icloud.com>
2023-12-15 14:07:34 +0100

    Update bundle_mac to use generated Info.plist

--- scripts/bundle_mac.sh
+++ scripts/bundle_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd "${MESON_SOURCE_ROOT}"
+cd "${MESON_BUILD_ROOT}"
 
 MESON_RES_PATH="${MESON_SOURCE_ROOT}/resources"
 MESON_LC_FR_PATH="${MESON_RES_PATH}/lang/fr/LC_MESSAGES"
@@ -18,7 +18,7 @@ mkdir "${APP_PATH}"
 
 install -d -m 755 "${CONTENTS_PATH}" "${RES_PATH}" "${EXE_PATH}" "${LC_FR_PATH}"
 
-install -m 644 "${MESON_RES_PATH}/Info.plist" "${CONTENTS_PATH}"
+install -m 644 "${MESON_BUILD_ROOT}/resources/Info.plist" "${CONTENTS_PATH}"
 install -m 644 "${MESON_RES_PATH}/conjugateur.icns" "${RES_PATH}"
 install -m 644 "${MESON_LC_FR_PATH}/wxstd.mo" "${LC_FR_PATH}"
 install -s -m 755 "${MESON_BUILD_ROOT}/conjugateur" "${EXE_PATH}"

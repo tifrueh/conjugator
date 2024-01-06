c8b7a774818f8c476677eb983f66e324711c9057
Timo Früh <timo.frueh@icloud.com>
2024-01-06 17:55:52 +0100

    Set scroll rate to 3

--- src/verbview.cpp
+++ src/verbview.cpp
@@ -22,7 +22,7 @@ VerbView::VerbView(wxWindow* parent, wxWindowID id, const verbDB::Verb &verb) : 
     setVerb(verb);
     
     this->SetVirtualSize(verbViewPanel->GetSize());
-    this->SetScrollRate(0, 4);
+    this->SetScrollRate(0, 3);
 
 }
 

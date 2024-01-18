eed19d453977528187da1d0a945f59bb2d991681
Timo Früh <timo.frueh@icloud.com>
2024-01-18 12:33:15 +0100

    Add documentation to verbview.cpp

--- src/verbview.cpp
+++ src/verbview.cpp
@@ -15,6 +15,7 @@
 
 #include "verbview.hpp"
 
+// Construct a new VerbView.
 VerbView::VerbView(wxWindow* parent, wxWindowID id, const verbDB::Verb &verb) : wxPanel(parent, id) {
     this->verb = verb;
     
@@ -22,6 +23,7 @@ VerbView::VerbView(wxWindow* parent, wxWindowID id, const verbDB::Verb &verb) : 
     
     tensebook = new wxChoicebook(this, wxID_ANY);
 
+    // Create a verb view panel for each tense from present to conditionnel and add it to the tensebook.
     for (int tense = verbDB::Tense::present; tense <= verbDB::Tense::conditionnel; tense++) {
         pages.insert({tense, new VerbViewPanel(tensebook, wxID_ANY, verb, tense)});
         tensebook->InsertPage(tense - verbDB::Tense::present, pages.at(tense), wxString(cjgt::getTense(tense)));
@@ -33,6 +35,7 @@ VerbView::VerbView(wxWindow* parent, wxWindowID id, const verbDB::Verb &verb) : 
 
 }
 
+// Set the verb of all verb view panels.
 void VerbView::setVerb(const verbDB::Verb &inputVerb) {
     verb = inputVerb;
     

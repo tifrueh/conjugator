29f7b2c18d13484978a3d8b01de1f654be3248c4
Timo Früh <timo.frueh@icloud.com>
2024-07-24 12:37:21 +0200

    Remove unnecessary i18n scripts

--- scripts/compile_mo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-
-msgfmt -o ../lang/fr/LC_MESSAGES/conjugator.mo ../lang/fr/LC_MESSAGES/conjugator.po
-msgfmt -o ../lang/de/LC_MESSAGES/conjugator.mo ../lang/de/LC_MESSAGES/conjugator.po

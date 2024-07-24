18e34a6d29fb38073709b22b91bfd5db4a599330
Timo Früh <timo.frueh@icloud.com>
2024-07-24 13:44:29 +0200

    Use correct mo/po paths when bundling

--- scripts/bundle_mac.sh
+++ scripts/bundle_mac.sh
@@ -5,8 +5,8 @@ cd "${MESON_BUILD_ROOT}"
 
 # Define various useful path variables.
 MESON_RES_PATH="${MESON_SOURCE_ROOT}/resources"
-MESON_LC_FR_PATH="${MESON_SOURCE_ROOT}/lang/fr/LC_MESSAGES"
-MESON_LC_DE_PATH="${MESON_SOURCE_ROOT}/lang/de/LC_MESSAGES"
+MESON_LC_FR_PATH="${MESON_BUILD_ROOT}/po/fr/LC_MESSAGES"
+MESON_LC_DE_PATH="${MESON_BUILD_ROOT}/po/de/LC_MESSAGES"
 
 APP_PATH="${MESON_BUILD_ROOT}/Conjugator.app"
 CONTENTS_PATH="${APP_PATH}/Contents"
@@ -29,8 +29,8 @@ install -d -m 755 "${CONTENTS_PATH}" "${RES_PATH}" "${EXE_PATH}" "${LC_FR_PATH}"
 # Install all necessary files to their correct locations inside the bundle.
 install -m 644 "${MESON_BUILD_ROOT}/configuration-files/Info.plist" "${CONTENTS_PATH}"
 install -m 644 "${MESON_RES_PATH}/conjugator.icns" "${RES_PATH}"
-install -m 644 "${MESON_LC_FR_PATH}/wxstd.mo" "${LC_FR_PATH}"
+install -m 644 "${MESON_RES_PATH}/mo/fr/LC_MESSAGES/wxstd.mo" "${LC_FR_PATH}"
 install -m 644 "${MESON_LC_FR_PATH}/conjugator.mo" "${LC_FR_PATH}"
-install -m 644 "${MESON_LC_DE_PATH}/wxstd.mo" "${LC_DE_PATH}"
+install -m 644 "${MESON_RES_PATH}/mo/de/LC_MESSAGES/wxstd.mo" "${LC_DE_PATH}"
 install -m 644 "${MESON_LC_DE_PATH}/conjugator.mo" "${LC_DE_PATH}"
 install -s -m 755 "${MESON_BUILD_ROOT}/conjugator" "${EXE_PATH}"
